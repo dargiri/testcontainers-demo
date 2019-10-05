a5613dc9aaee67d8d4cd617e1ff40ed33eba5b9f
Dionis Argiri <dionis.argiri@gmail.com>
2019-10-06 02:25:12 +0300

    Dumb usage of multiple containers using testcontainers.org

--- src/test/java/com/example/demo/ITConfig.java
+++ src/test/java/com/example/demo/ITConfig.java
@@ -5,6 +5,7 @@ import com.zaxxer.hikari.HikariDataSource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
 
 import javax.sql.DataSource;
@@ -17,19 +18,25 @@ public class ITConfig {
     public PostgresDockerContainerWrapper postgresDockerContainerWrapper() {
         return new PostgresDockerContainerWrapper();
     }
+    @Bean
+    public MysqlDockerContainerWrapper mysqlDockerContainerWrapper() {
+        return new MysqlDockerContainerWrapper();
+    }
 
     @Bean
-    public DataSource dataSource(PostgresDockerContainerWrapper postgresDockerContainerWrapper) {
-        PostgreSQLContainer dbContainer = postgresDockerContainerWrapper.getDbContainer();
+    public DataSource dataSource(PostgresDockerContainerWrapper postgresDockerContainerWrapper, MysqlDockerContainerWrapper mysqlDockerContainerWrapper) {
+        PostgreSQLContainer pgDbContainer = postgresDockerContainerWrapper.getDbContainer();
+        log.info("Will create PG Datasource: JdbcURL: {}, username: {}, pass: {}, driver class: {}", pgDbContainer.getJdbcUrl(), pgDbContainer.getUsername(), pgDbContainer.getPassword(), pgDbContainer.getDriverClassName());
 
-        log.info("Will create Datasource: JdbcURL: {}, username: {}, pass: {}, driver class: {}", dbContainer.getJdbcUrl(), dbContainer.getUsername(), dbContainer.getPassword(), dbContainer.getDriverClassName());
+        MySQLContainer mysqlDbContainer = mysqlDockerContainerWrapper.getDbContainer();
+        log.info("Will create MySQL Datasource: JdbcURL: {}, username: {}, pass: {}, driver class: {}", mysqlDbContainer.getJdbcUrl(), mysqlDbContainer.getUsername(), mysqlDbContainer.getPassword(), mysqlDbContainer.getDriverClassName());
 
         HikariConfig dsConfig = new HikariConfig();
 
-        dsConfig.setJdbcUrl(dbContainer.getJdbcUrl());
-        dsConfig.setUsername(dbContainer.getUsername());
-        dsConfig.setPassword(dbContainer.getPassword());
-        dsConfig.setDriverClassName(dbContainer.getDriverClassName());
+        dsConfig.setJdbcUrl(pgDbContainer.getJdbcUrl());
+        dsConfig.setUsername(pgDbContainer.getUsername());
+        dsConfig.setPassword(pgDbContainer.getPassword());
+        dsConfig.setDriverClassName(pgDbContainer.getDriverClassName());
 
         return new HikariDataSource(dsConfig);
     }
---
+++ src/test/java/com/example/demo/MysqlDockerContainerWrapper.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.containers.MySQLContainerProvider;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+@Slf4j
+public class MysqlDockerContainerWrapper {
+    @Getter
+    private final MySQLContainer dbContainer = new MySQLContainer("mysql:5.7.22");
+
+    @PostConstruct
+    void start() {
+        log.info("Starting mysql Docker test container");
+        dbContainer.start();
+        log.info("Started mysql Docker test container");
+    }
+
+    @PreDestroy
+    void stop() {
+        log.info("Stopping mysql Docker test container");
+        dbContainer.stop();
+        log.info("Stopped mysql Docker test container");
+    }
+}
